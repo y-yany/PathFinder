d586d04c3f93bcc8b84eed02c2637238281ae7e2
yuya kobayashi <160720517+y-yany@users.noreply.github.com.>
2024-12-19 23:14:59 +0900

    Add:カスタムテーマの設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,24 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  plugins: [
+    require('daisyui')
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#24C1C7",
+          "secondary": "#cffafe",
+          "accent": "#FC8A26",
+          "neutral": "#2F5461",
+          "info": "#67e8f9",
+          "success": "#6ee7b7",
+          "warning": "#fde047",
+          "error": "#fda4af",
+        },
+      },
+    ],
+  },
 }
