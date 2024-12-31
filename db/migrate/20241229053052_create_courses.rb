b690cdfa0d5049e35e1d9c5090ad6a9d6eed1886
yuya kobayashi <160720517+y-yany@users.noreply.github.com.>
2024-12-31 17:09:14 +0900

    Fix:カラムの制約を修正

--- db/migrate/20241229053052_create_courses.rb
+++ db/migrate/20241229053052_create_courses.rb
@@ -1,11 +1,11 @@
 class CreateCourses < ActiveRecord::Migration[7.0]
   def change
     create_table :courses do |t|
-      t.string :title,                            null: false, default: ""
-      t.text :body
+      t.string :title,                            null: false
+      t.text :body,                                            default: ""
       t.decimal :distance, precision: 5, scale: 2
-      t.string :address
-      t.string :encoded_polyline,                 null: false, default: ""
+      t.string :address,                                       default: ""
+      t.string :encoded_polyline,                 null: false
       t.references :user, foreign_key: true
 
       t.timestamps
