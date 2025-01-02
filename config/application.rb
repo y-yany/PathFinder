21450f6ee05af7005c2c8ab030751dc82173d2d9
yuya kobayashi <160720517+y-yany@users.noreply.github.com.>
2025-01-03 01:03:27 +0900

    Add:eager_load_pathsの追加

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module Myapp
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.eager_load_paths << Rails.root.join("app/types")
 
     # Don't generate system test files.
     config.generators.system_tests = nil
