1df5825e380a10ad40e135bd3c661a02c81ee8f1
yuya kobayashi <160720517+y-yany@users.noreply.github.com.>
2025-01-02 16:58:25 +0900

    Fix:Lintエラーの修正

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,17 +1,17 @@
 class CoursesController < ApplicationController
-	def create
-		@course_marker_form = CourseMarkerForm.new(course_marker_params)
-		if @course_marker_form.save
-			redirect_to root_path, success: 'コースを作成しました'
-		else
-			flash.now[:error] = "コースを作成できませんでした"
-			render 'new', status: :unprocessable_entity
-		end
-	end
+  def create
+    @course_marker_form = CourseMarkerForm.new(course_marker_params)
+    if @course_marker_form.save
+      redirect_to root_path, success: "コースを作成しました"
+    else
+      flash.now[:error] = "コースを作成できませんでした"
+      render :new, status: :unprocessable_entity
+    end
+  end
 
-	private
+  private
 
-	def course_marker_params
-		params.require(:course_marker_form).permit(:title, :body, :distance, :address, :encoded_polyline, positions: []).merge(user_id: current_user.id)
-	end
+  def course_marker_params
+    params.require(:course_marker_form).permit(:title, :body, :distance, :address, :encoded_polyline, positions: []).merge(user_id: current_user.id)
+  end
 end
