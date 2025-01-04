014b755c45340a2d3415bec7b5dc2315a6d5b8e3
yuya kobayashi <160720517+y-yany@users.noreply.github.com.>
2025-01-04 12:23:40 +0900

    Add:コース作成画面にマップを表示

--- app/javascript/gmap.js
+++ app/javascript/gmap.js
@@ -4,10 +4,18 @@ async function initMap() {
   // 必要なライブラリをインポート
   const { Map } = await google.maps.importLibrary("maps");
 
-  map = new Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
+  // マップのオプションを設定
+  const defaultLocation = { lat: 35.6809591, lng: 139.7673068 };
+  const mapOptions = {
+    center: { lat: defaultLocation.lat, lng: defaultLocation.lng },
+    zoom: 15,
+    streetViewControl: false, // ストリートビューのボタン非表示
+    mapTypeControl: false, // 地図、航空写真のボタン非表示
+    fullscreenControl: false, // フルスクリーンボタン非表示
+  };
+
+  // マップオブジェクトの作成
+  map = new Map(document.getElementById("create_map"), mapOptions);
 }
 
 initMap();
