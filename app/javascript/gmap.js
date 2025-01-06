96836408863c4839e11c4fa1f9a1b3578b15ea57
yuya kobayashi <160720517+y-yany@users.noreply.github.com.>
2025-01-07 02:11:48 +0900

    Add:定義した関数のリファクタリング

--- app/javascript/gmap.js
+++ app/javascript/gmap.js
@@ -1,10 +1,7 @@
 let map;
-let routePoints = [];
+let routePoints = []; // ルートを構成する座標データ
 let directionsService;
-let routePolyline;
-let totalDistance = 0;
-let startAddress;
-let encodedPolyline;
+let routePolyline; // ルートのポリラインオブジェクト
 
 async function initMap() {
   // 必要なライブラリをインポート
@@ -31,60 +28,74 @@ async function initMap() {
     if (!e.placeId) {
       routePoints.push(e.latLng);
     }
-    calcRoute();
-  });
 
-  // フォームに値を追加
-  addValueToForm();
+    if (routePoints.length >= 2) {
+      resetPolyline(routePolyline); // マップに描写されているポリラインを削除
+
+      calcRoute(routePoints) // ルートを計算
+        .then(route => {
+          const encodedPolyline = getEncodedPolyline(route); // ポリラインデータを取得
+          drawPolyline(encodedPolyline); // ポリラインをマップ上に描写
+          addValueToForm(route, routePoints); // フォームにデータを追加
+        });
+    }
+  });
 }
 
 initMap();
 
 // !ルート計算
-function calcRoute() {
-  if (routePoints.length < 2) {
+function calcRoute(routePoints) {
+  // ルートの条件
+  const start = routePoints[0];
+  const end = routePoints[routePoints.length - 1];
+  const waypts = routePoints.slice(1, -1).map(point => ({
+    location: point,
+    stopover: true,
+  }));
+  const request = {
+    origin: start,
+    destination: end,
+    waypoints: waypts,
+    travelMode: 'WALKING',
+  };
+
+  if (waypts.length > 10) {
     return;
-  } else {
-    resetPolyline(routePolyline); // ポリラインのリセット
-
-    // ルートの条件
-    const start = routePoints[0];
-    const end = routePoints[routePoints.length - 1];
-    const waypts = routePoints.slice(1, -1).map(point => ({
-      location: point,
-      stopover: true,
-    }));
-    const request = {
-      origin: start,
-      destination: end,
-      waypoints: waypts,
-      travelMode: 'WALKING',
-    };
-
-    // 計算
-    if (waypts.length > 10) {
-      return;
-    } else {
-      directionsService.route(request, (response, status) => {
-        if (status == 'OK') {
-          const route = response.routes[0]; // ルート計算の結果からルート情報を取得
-          encodedPolyline = route.overview_polyline; // ルートのポリラインデータを取得
-          drawPolyline(encodedPolyline);
-          calcTotalDistance(route);
-          startAddress = route.legs[0].start_address; // ルートの始点の住所を取得
-        }
-      });
-    }
   }
+  
+  // 計算
+  return new Promise((resolve) => {
+    directionsService.route(request, (response, status) => {
+      if (status == 'OK') {
+        const route = response.routes[0]; // ルート計算の結果からルート情報を取得
+        resolve(route);
+      }
+    });
+  })
 }
 window.calcRoute = calcRoute;
+
+// !ポリラインデータを取得
+function getEncodedPolyline(route) {
+  const encodedPolyline = route.overview_polyline; // ルートのポリラインデータを取得
+  
+  return encodedPolyline;
+}
+window.getEncodedPolyline = getEncodedPolyline;
 
 // !ポリラインデータをマップ上に描写
 function drawPolyline(encodedPolyline) {
   const routeCoordinates = google.maps.geometry.encoding.decodePath(encodedPolyline); // エンコードされたパスをデコード
 
-  // ポリラインオブジェクトの作成
-  routePolyline = new google.maps.Polyline({
+  routePolyline = getPolylineObject(routeCoordinates); // ポリラインオブジェクトを作成
+  routePolyline.setMap(map);
+}
+window.drawPolyline = drawPolyline;
+
+// !ポリラインオブジェクトの作成
+function getPolylineObject(routeCoordinates) {
+  const polylineObject = new google.maps.Polyline({
     path: routeCoordinates,
     geodesic: true, // 地球の曲率を考慮した直線
     strokeColor: "#ff7f50", // ポリラインの色
@@ -92,9 +103,9 @@ function drawPolyline(encodedPolyline) {
     strokeWeight: 7, // ポリラインの太さ
   });
 
-  routePolyline.setMap(map);
+  return polylineObject;
 }
-window.drawPolyline = drawPolyline;
+window.getPolylineObject = getPolylineObject;
 
 // !ポリラインをマップ上から除去
 function resetPolyline(routePolyline) {
@@ -107,28 +118,37 @@ window.resetPolyline = resetPolyline;
 
 // !ルートの総距離を計算
 function calcTotalDistance(route) {
+  let totalDistance = 0;
+
   route.legs.forEach((leg) => {
     totalDistance += leg.distance.value / 1_000; // 総距離kmを算出
   });
+
+  return totalDistance;
 }
 window.calcTotalDistance = calcTotalDistance;
 
 // !フォームに値を追加
-function addValueToForm() {
+function addValueToForm(route, routePoints) {
   const form = document.getElementById("course-form");
-  form.addEventListener('submit', (e) => {
-    // コースの距離
-    const distanceField = e.target.querySelector('#course-distance-field');
-    distanceField.value = totalDistance;
-
-    // コースの始点の住所
-    const addressField = e.target.querySelector('#course-address-field');
-    addressField.value = startAddress;
-    
-    // ポリラインデータ
-    const encodedPolylineField = e.target.querySelector('#course-encoded-polyline-field');
-    encodedPolylineField.value = encodedPolyline;
-    // console.log(e.target);
-  })
+  if (form) {
+    form.addEventListener('submit', (e) => {
+      // コースの距離
+      const distanceField = e.target.querySelector('#course-distance-field');
+      distanceField.value = calcTotalDistance(route);
+  
+      // コースの始点の住所
+      const addressField = e.target.querySelector('#course-address-field');
+      addressField.value = route.legs[0].start_address;
+      
+      // ポリラインデータ
+      const encodedPolylineField = e.target.querySelector('#course-encoded-polyline-field');
+      encodedPolylineField.value = getEncodedPolyline(route);
+  
+      // コースを構成する座標データ
+      const positionsField = e.target.querySelector('#marker-positions-field');
+      positionsField.value = JSON.stringify(routePoints);
+    })
+  }
 }
 window.addValueToForm = addValueToForm;
