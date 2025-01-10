e258d1df747ef8488c949f097a7213bc6a025b8a
yuya kobayashi <160720517+y-yany@users.noreply.github.com.>
2025-01-10 12:33:47 +0900

    Add:認証後のリダイレクト先をコース一覧画面に変更

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -52,7 +52,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
   def after_sign_up_path_for(_resource)
-    root_path
+    courses_path
   end
 
   # The path used after sign up for inactive accounts.
