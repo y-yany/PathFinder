748bd6082e13ad200747a584f5107bfd0394e91e
yuya kobayashi <160720517+y-yany@users.noreply.github.com.>
2025-02-15 17:51:28 +0900

    Add:Swiperの導入およびセットアップ

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,18 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+// import Swiper JS
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
+// init Swiper
+document.addEventListener("turbo:load", () => {
+  const swiper = new Swiper('.swiper', {  
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+});
