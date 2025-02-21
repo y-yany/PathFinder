aca23caad63709483fbf881631e8fab2146c7d2b
yuya kobayashi <160720517+y-yany@users.noreply.github.com.>
2025-02-21 17:38:07 +0900

    Add:通常フォームから新規登録時、uidに一意な値を設定

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -40,6 +40,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   protected
 
+  def build_resource(hash = {})
+    # ユーザーオブジェクト作成時、uidに一意な値を設定
+    hash[:uid] = SecureRandom.uuid
+    super
+  end
+
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
