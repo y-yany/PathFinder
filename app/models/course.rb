220e59128359bad1f14621208259c5e96536227e
yuya kobayashi <160720517+y-yany@users.noreply.github.com.>
2025-02-24 12:27:59 +0900

    Add:Courseモデルにバリデーションを設定

--- app/models/course.rb
+++ app/models/course.rb
@@ -10,4 +10,5 @@ class Course < ApplicationRecord
   validates :distance, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 999.99 }
   validates :address, length: { maximum: 255 }
   validates :encoded_polyline, presence: true, length: { maximum: 255 }
+  validates :main_images, attachment: { purge: true, content_type: %r{\Aimage/(png|jpeg)\Z}, maximum: 5_242_880 }
 end
