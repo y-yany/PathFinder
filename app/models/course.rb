9e6da5a2b9d33d082e3b7233eb3096ae83190cf3
yuya kobayashi <160720517+y-yany@users.noreply.github.com.>
2025-03-30 11:42:33 +0900

    Add:Courseモデルのモデルスペックを実装

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id                   :bigint           not null, primary key
+#  user_id              :bigint
+#  title                :string(255)      not null
+#  body                 :text             default("")
+#  distance             :decimal
+#  address              :string(255)      default("")
+#  encoded_polyline     :text             not null
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#
+# Indexes
+#
+#  index_courses_on_user_id     (user_id)
+#
+
 class Course < ApplicationRecord
   belongs_to :user
   has_many :markers, dependent: :destroy
@@ -10,7 +29,7 @@ class Course < ApplicationRecord
   validates :body, length: { maximum: 1_000 }
   validates :distance, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 999.99 }
   validates :address, length: { maximum: 255 }
-  validates :encoded_polyline, presence: true, length: { maximum: 255 }
+  validates :encoded_polyline, presence: true, length: { maximum: 65_535 }
   validates :main_images, attachment: { purge: true, content_type: %r{\Aimage/(png|jpeg)\Z}, maximum: 5_242_880 }
 
   scope :title_body_address_contain, ->(word) { where("title LIKE ? OR body LIKE ? OR address LIKE ?", "%#{word}%", "%#{word}%", "%#{word}%") }
