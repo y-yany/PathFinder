0cb35f7d593a1a6ea82cc883bcc5817e0d38dfc0
yuya kobayashi <160720517+y-yany@users.noreply.github.com.>
2025-04-08 20:34:39 +0900

    Update:daisyuiのテーマを変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,19 +9,6 @@ module.exports = {
     require('daisyui')
   ],
   daisyui: {
-    themes: [
-      {
-        mytheme: {
-          "primary": "#24C1C7",
-          "secondary": "#cffafe",
-          "accent": "#FC8A26",
-          "neutral": "#2F5461",
-          "info": "#67e8f9",
-          "success": "#6ee7b7",
-          "warning": "#fde047",
-          "error": "#fda4af",
-        },
-      },
-    ],
+    themes: ["corporate"],
   },
 }
