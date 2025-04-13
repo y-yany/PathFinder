0bab6373477ae90f7b7cf4cb703463548e2297a6
yuya kobayashi <160720517+y-yany@users.noreply.github.com.>
2025-04-13 17:06:30 +0900

    Update:コース詳細画面の修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,10 +6,24 @@ import "./controllers"
 import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
 // init Swiper
 document.addEventListener("turbo:load", () => {
-  const swiper = new Swiper('.swiper', {
+  const courseIndexSwiper = new Swiper('.courseIndexSwiper', {
     pagination: {
       el: ".swiper-pagination",
       dynamicBullets: true,
+    },
+  });
+
+  const courseShowSwiper = new Swiper('.courseShowSwiper', {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
     },
   });
 });
