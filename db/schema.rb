81bcc25b3dae33ba300f7f778781674e0f696dbf
yuya kobayashi <160720517+y-yany@users.noreply.github.com.>
2025-05-30 21:41:09 +0900

    Add:likesテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_02_20_131601) do
+ActiveRecord::Schema[7.0].define(version: 2025_05_30_100326) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "postgis"
@@ -55,6 +55,16 @@ ActiveRecord::Schema[7.0].define(version: 2025_02_20_131601) do
     t.index ["user_id"], name: "index_courses_on_user_id"
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "course_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_likes_on_course_id"
+    t.index ["user_id", "course_id"], name: "index_likes_on_user_id_and_course_id", unique: true
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
   create_table "markers", force: :cascade do |t|
     t.geography "location", limit: {:srid=>4326, :type=>"st_point", :geographic=>true}, null: false
     t.integer "order"
@@ -88,5 +98,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_02_20_131601) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "courses", "users"
+  add_foreign_key "likes", "courses"
+  add_foreign_key "likes", "users"
   add_foreign_key "markers", "courses"
 end
