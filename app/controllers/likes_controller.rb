6a447632d7a01cb3e716138479436da2955c137f
yuya kobayashi <160720517+y-yany@users.noreply.github.com.>
2025-06-04 22:57:38 +0900

    Add:いいねボタンの追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,12 +2,12 @@ class LikesController < ApplicationController
   def create
     like = current_user.likes.build(course_id: params[:course_id])
     like.save
-    redirect_to courses_path
+    redirect_to request.referer || courses_path
   end
 
   def destroy
     like = Like.find(params[:id])
     like.destroy!
-    redirect_to courses_path, status: :see_other
+    redirect_to request.referer || courses_path, status: :see_other
   end
 end
