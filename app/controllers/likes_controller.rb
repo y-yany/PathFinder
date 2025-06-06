82cd60e668dab58fb55a24b063aa9d682060afe9
yuya kobayashi <160720517+y-yany@users.noreply.github.com.>
2025-06-06 13:18:30 +0900

    Update:いいね機能に関する処理を修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,13 @@
 class LikesController < ApplicationController
   def create
-    like = current_user.likes.build(course_id: params[:course_id])
-    like.save
+    @course = Course.find(params[:course_id])
+    current_user.like(@course)
     redirect_to request.referer || courses_path
   end
 
   def destroy
-    like = Like.find(params[:id])
-    like.destroy!
+    @course = Like.find(params[:id]).course
+    current_user.unlike(@course)
     redirect_to request.referer || courses_path, status: :see_other
   end
 end
