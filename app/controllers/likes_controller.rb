67bba9e42084e2ab2a40d3ca07cc8cc9e8ccc719
yuya kobayashi <160720517+y-yany@users.noreply.github.com.>
2025-06-06 15:11:49 +0900

    Add:likesコントローラを修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,12 +2,10 @@ class LikesController < ApplicationController
   def create
     @course = Course.find(params[:course_id])
     current_user.like(@course)
-    redirect_to request.referer || courses_path
   end
 
   def destroy
     @course = Like.find(params[:id]).course
     current_user.unlike(@course)
-    redirect_to request.referer || courses_path, status: :see_other
   end
 end
