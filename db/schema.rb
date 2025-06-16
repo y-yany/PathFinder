ae340db6efcc0ef43aa8dd930998d3ef4e5332bc
yuya kobayashi <160720517+y-yany@users.noreply.github.com.>
2025-06-16 10:53:44 +0900

    Add:usersテーブルにカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_06_07_005151) do
+ActiveRecord::Schema[7.0].define(version: 2025_06_16_014929) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "postgis"
@@ -50,7 +50,6 @@ ActiveRecord::Schema[7.0].define(version: 2025_06_07_005151) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["course_id"], name: "index_comments_on_course_id"
-    t.index ["user_id", "course_id"], name: "index_comments_on_user_id_and_course_id", unique: true
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
@@ -100,6 +99,8 @@ ActiveRecord::Schema[7.0].define(version: 2025_06_07_005151) do
     t.datetime "updated_at", null: false
     t.string "provider"
     t.string "uid"
+    t.string "avatar"
+    t.text "profile"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
